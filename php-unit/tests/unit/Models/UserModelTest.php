eb9269b0fa80030f3c79418d480dbba523c216e9
Luidson Lima <luidsonlsti@gmail.com>
2025-10-16 20:42:09 -0300

    refact: split user model mock from user model test

--- php-unit/tests/unit/Models/UserModelTest.php
+++ php-unit/tests/unit/Models/UserModelTest.php
@@ -2,68 +2,21 @@
 
 namespace Tests;
 
-use App\Enums\Role;
-use App\Enums\Status;
 use App\Models\User;
 use PHPUnit\Framework\TestCase;
-use Faker\Factory;
+use Tests\mocks\Models\UserMocker;
 
 final class UserModelTest extends TestCase
 {
-    private User $user;
-    private \Faker\Generator $faker;
-
-    public function setUp(): void
-    {
-        $this->faker = Factory::create();
-    }
-
-    private function mockUserModel(bool $generatePassword = true) : User
-    {
-        $params =[
-            'firstName' => $this->faker->firstName(),
-            'lastName' => $this->faker->lastName(),
-            'email' => $this->faker->email(),
-            'role' => $this->faker->randomElement(Role::cases()),
-            'status' => $this->faker->randomElement(Status::cases())
-        ];
-
-        if($generatePassword)
-        {
-            $params['password'] = password_hash($this->faker->password(), PASSWORD_BCRYPT);
-            
-            $user = new User(
-                firstName: $params['firstName'],
-                lastName: $params['lastName'],
-                email: $params['email'],
-                role: $params['role'],
-                status: $params['status'],
-                password: $params['password'],
-            );
-
-            return $user;
-        }
-
-        $user = new User(
-            firstName: $params['firstName'],
-            lastName: $params['lastName'],
-            email: $params['email'],
-            role: $params['role'],
-            status: $params['status']
-        );
-
-        return $user;
-    }
-
     public function testUserWithPassword()
     {
-        $user = $this->mockUserModel(generatePassword: true);
+        $user = UserMocker::mockUserModel(generatePassword: true);
         $this->assertInstanceOf(User::class, $user);
     }
 
     public function testUserWithoutPassword()
     {
-        $user = $this->mockUserModel(generatePassword: false);
+        $user = UserMocker::mockUserModel(generatePassword: false);
         $this->assertInstanceOf(User::class, $user);
     }
 }
